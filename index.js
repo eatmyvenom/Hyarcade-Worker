e0774e5c8515ddfa9f7e15496f927d3eb0c3fd40
eatmyvenom <eat.my.venomm@gmail.com>
2021-09-15 01:25:22 -0700

    add support for proxying hypixel api connections

--- index.js
+++ index.js
@@ -9,7 +9,32 @@ addEventListener("fetch", (event) => {
  * @returns {Response}
  */
 async function handleRequest (request) {
-  return new Response("Hello worker!", {
-    headers: { "content-type": "text/plain" },
+  // eslint-disable-next-line no-undef
+  const realPass = await CONFIG.get("pass");
+  const uuids = request.headers.get("accs").split(",");
+  const key = request.headers.get("apikey");
+  const url = new URL(request.url);
+  const pass = url.searchParams.get("pass");
+  
+  // eslint-disable-next-line no-undef
+  if(pass != realPass) {
+    return new Response(JSON.stringify({ error: "INVALID-AUTH", sent: pass }));
+  }
+
+  const accs = [];
+
+  const init = {
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+    },
+  };
+
+  for(const uuid of uuids) {
+    const acc = await fetch(`https://api.hypixel.net/player?key=${key}&uuid=${uuid}`, init);
+    accs.push(await acc.json());
+  }
+
+  return new Response(JSON.stringify(accs), {
+    headers: { "content-type": "application/json" },
   });
 }
